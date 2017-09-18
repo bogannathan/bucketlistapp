0799a2770199f32de86297023e7af16c9eaf505d
Nathan Bogan <bogannathan@gmail.com>
2017-09-18 13:25:56 -0400

    added test routes

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const app = express();
+const router = require('./router');
 
 app.use(bodyParser.json({ type: '*/*'}));
+router(app);
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
