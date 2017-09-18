6586cf0037fb96a565b6b892003fd0377e2325a2
Nathan Bogan <bogannathan@gmail.com>
2017-09-18 17:40:01 -0400

    added token to signup

--- server/router.js
+++ server/router.js
@@ -1,7 +1,12 @@
-let Auth = require('./controllers/auth');
-let User = require('./models/user');
-
+const Auth = require('./controllers/auth');
+const passportService = require('./services/passport');
+const passport = require('passport');
+let requireAuth = passport.authenticate('jwt', {session: false});
 
 module.exports = function(app) {
+
+	app.get('/', requireAuth, function(req, res){
+		res.send('hello homepage');
+	})
 	app.post('/signup', Auth.signup)	
 }
