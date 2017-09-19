a3978c31de0b08dedf41b1c555c153420a436487
Nathan Bogan <bogannathan@gmail.com>
2017-09-19 09:58:41 -0400

    added nav bar

---
+++ client/src/components/nav.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {Navbar, Nav, NavItem, NavDropdown, DropdownButton, MenuItem, CollapsibleNav} from 'react-bootstrap';
+
+class NavBarHeader extends Component {
+	render() {
+		return (
+			<Navbar>
+			    <Navbar.Header>
+			      <Navbar.Brand>
+			        <a href="#">Bucket List</a>
+			      </Navbar.Brand>
+			    </Navbar.Header>
+			    <Nav>
+			      <NavItem eventKey={1} href="#">Sign in</NavItem>
+			      <NavItem eventKey={2} href="#">Sign up</NavItem>
+			      <NavDropdown eventKey={3} title="Other stuffs" id="basic-nav-dropdown">
+			        <MenuItem eventKey={3.1}>Action</MenuItem>
+			        <MenuItem eventKey={3.2}>Another action</MenuItem>
+			        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+			        <MenuItem divider />
+			        <MenuItem eventKey={3.4}>Separated link</MenuItem>
+			      </NavDropdown>
+			    </Nav>
+			  </Navbar>
+		)
+	}
+}
+
+export default NavBarHeader;
