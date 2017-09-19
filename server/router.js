ae9061df10f701d8129fdbd093bea5b2edb4b1e5
Nathan Bogan <bogannathan@gmail.com>
2017-09-18 20:11:50 -0400

    added sign in functionality

--- server/router.js
+++ server/router.js
@@ -2,11 +2,13 @@ const Auth = require('./controllers/auth');
 const passportService = require('./services/passport');
 const passport = require('passport');
 let requireAuth = passport.authenticate('jwt', {session: false});
-
+let requireSignin = passport.authenticate('local', {session: false});
 module.exports = function(app) {
 
 	app.get('/', requireAuth, function(req, res){
 		res.send('hello homepage');
 	})
 	app.post('/signup', Auth.signup)	
+
+	app.post('/signin', requireSignin, Auth.signin);
 }
